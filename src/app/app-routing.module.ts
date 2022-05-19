aa1408f40e731092c47b2b7d7d9089732638106d
Manav <manav.bansal@affordplan.com>
2022-05-19 12:26:08 +0530

    full app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoansTypesComponent } from './loans-types/loans-types.component';
+import { LoansComponent } from './loans/loans.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'loans',
+    component:LoansComponent
+  },
+  {
+    path:'loans-types',
+    component:LoansTypesComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
 import { FormsModule } from '@angular/forms';
+import { LoansComponent } from './loans/loans.component';
+import { LoansTypesComponent } from './loans-types/loans-types.component';
+// import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective
+    HighlightDirective,
+    LoansComponent,
+    LoansTypesComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [
+    // {provide: LocationStrategy, useClass:HashLocationStrategy}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
